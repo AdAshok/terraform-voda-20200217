5cb2a82ca0051c4e1dd2297c23071b63ef6eaa39
root <root@terraform>
2020-02-18 14:05:41 +0000

    AWS CLI

--- 13-Backend-and-Remote-state/backend-remote-s3.tf
+++ 13-Backend-and-Remote-state/backend-remote-s3.tf
@@ -1,15 +1,19 @@
+# Use AWSCLI instead of defining Keys here
+
 terraform {   
     backend "s3" { 
      bucket = "terraform-demo-amitvashist"     
       key    = "network/terraform.tfstate"     
       region = "us-east-2"   
+#      access_key = ""
+#      secret_key = "" 
     } 
 }
 
 provider "aws" {
   region = "us-east-2"
-  access_key = "XXXXX"
-  secret_key = "XXXXX" 
+ # access_key = ""
+ # secret_key = "" 
 }
 
 resource "aws_instance" "example" {
